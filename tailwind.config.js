53d508d9f077201f3e219882df6ef644c2b5d4bc
mbravosarria <mbravosarria@gmail.com>
2023-03-27 19:11:35 -0400

    build: install and configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  // Ensure these match with .storybook/preview.js
+  theme: {
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
+  },
+  plugins: [],
+};
