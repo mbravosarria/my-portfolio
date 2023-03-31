4f1483383a4ac628509b7cdfcd8f2de2637187d2
mbravosarria <mbravosarria@gmail.com>
2023-03-31 14:21:57 -0400

    feat: add description tooltip to experience card component

--- src/components/cards/experience/ExperienceCard.tsx
+++ src/components/cards/experience/ExperienceCard.tsx
@@ -1,3 +1,5 @@
+import { Tooltip } from 'react-tooltip';
+
 export interface IExperienceCard {
   date: string;
   role: string;
@@ -16,10 +18,28 @@ const ExperienceCard: React.FC<IExperienceCard> = ({
   return (
     <div className="flex items-start justify-start space-x-8">
       <h4 className="font-semibold text-secondary-color">{date}</h4>
-      <div>
+      <div className="experience-info cursor-pointer">
         <h5 className="font-semibold text-black-color">{role}</h5>
         <p className="text-gray-color">{company}</p>
+        <div className="flex flex-wrap gap-2">
+          {tags.map((item, index) => (
+            <p key={index} className="text-brown-color">
+              #{item}
+            </p>
+          ))}
+        </div>
       </div>
+      <Tooltip
+        anchorSelect=".experience-info"
+        place="top"
+        style={{
+          backgroundColor: '#fff',
+          boxShadow: '0 10px 15px 0 rgba(0, 0, 0, 0.3)',
+          width: 300,
+        }}
+      >
+        <p className="mb-2 text-black-color">{description}</p>
+      </Tooltip>
     </div>
   );
 };
