75496f67aa1eca00cc6d881e903cde026b73471a
mbravosarria <mbravosarria@gmail.com>
2023-04-02 14:12:48 -0400

    fix: remove description tooltip from experience card component

--- src/components/cards/experience/ExperienceCard.mocks.ts
+++ src/components/cards/experience/ExperienceCard.mocks.ts
@@ -4,8 +4,6 @@ const base: IExperienceCard = {
   date: 'Proident nostrud',
   role: 'Dolor aliqua non quis cillum irure enim veniam pariatur aliqua.',
   company: 'Aliquip nisi ad do laboris in non velit sit proident ut voluptate.',
-  description:
-    'Non occaecat dolor nisi exercitation proident id mollit excepteur irure excepteur velit nisi. Minim esse et nisi dolore mollit quis. Quis pariatur dolor adipisicing fugiat eiusmod officia anim mollit do. Laborum sunt duis proident excepteur id sunt proident cillum qui.',
   tags: ['tailwindcss', 'eCommerce', 'nodejs', 'react', 'next js'],
 };
 
--- src/components/cards/experience/ExperienceCard.tsx
+++ src/components/cards/experience/ExperienceCard.tsx
@@ -1,10 +1,8 @@
-import { Tooltip } from 'react-tooltip';
 
 export interface IExperienceCard {
   date: string;
   role: string;
   company: string;
-  description: string;
   tags: Array<string>;
 }
 
@@ -12,13 +10,12 @@ const ExperienceCard: React.FC<IExperienceCard> = ({
   date,
   role,
   company,
-  description,
   tags,
 }) => {
   return (
     <div className="flex items-start justify-start space-x-8">
       <h4 className="font-semibold text-secondary-color">{date}</h4>
-      <div className="experience-info cursor-pointer">
+      <div className="cursor-pointer">
         <h5 className="font-semibold text-black-color">{role}</h5>
         <p className="text-gray-color">{company}</p>
         <div className="flex flex-wrap gap-2">
@@ -29,17 +26,6 @@ const ExperienceCard: React.FC<IExperienceCard> = ({
           ))}
         </div>
       </div>
-      <Tooltip
-        anchorSelect=".experience-info"
-        place="top"
-        style={{
-          backgroundColor: '#fff',
-          boxShadow: '0 10px 15px 0 rgba(0, 0, 0, 0.3)',
-          width: 300,
-        }}
-      >
-        <p className="mb-2 text-black-color">{description}</p>
-      </Tooltip>
     </div>
   );
 };
