bcf0754d8b61976ff27fbc2d6d5d0599fa08c8bf
mbravosarria <mbravosarria@gmail.com>
2023-04-02 11:05:06 -0400

    feat: sanity schemas definition

---
+++ backend_sanity/schemas/experience.ts
@@ -0,0 +1,39 @@
+export default {
+  name: 'experience',
+  title: 'Experience',
+  type: 'document',
+  fields: [
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'string',
+    },
+    {
+      name: 'role',
+      title: 'Role',
+      type: 'string',
+    },
+    {
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [
+        {
+          name: 'tag',
+          title: 'Tag',
+          type: 'string',
+        },
+      ],
+    },
+  ],
+}
--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import {default as about} from './about'
+import {default as contact} from './contact'
+import {default as experience} from './experience'
+import {default as skills} from './skills'
+import {default as works} from './works'
+
+export const schemaTypes = [about, contact, experience, skills, works]
