c84e411fc0fb9d5e15221fa6e40303a7634bb2a0
mbravosarria <mbravosarria@gmail.com>
2023-04-04 13:58:13 -0400

    feat: footer dection | send message

--- src/components/sections/footer/Footer.tsx
+++ src/components/sections/footer/Footer.tsx
@@ -1,6 +1,43 @@
+import { client } from '@/lib/client';
+import { useState } from 'react';
+
 export interface IFooter {}
 
 const Footer: React.FC<IFooter> = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+    };
+
+    client
+      .create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <section
       id="contact"
@@ -9,31 +46,44 @@ const Footer: React.FC<IFooter> = () => {
       <h2 className="text-xl font-bold text-black-color sm:text-4xl md:text-5xl">
         Take A Coffee & Chat With Me
       </h2>
-      <div className="mt-8 flex w-full flex-col items-center justify-center py-4 px-8 md:w-3/5">
-        <input
-          className="my-3 w-full cursor-text rounded-xl border-0 bg-primary-color p-3 text-secondary-color outline-none transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-primary-color"
-          type="text"
-          name="name"
-          placeholder="Your Name"
-        />
-        <input
-          className="my-3 w-full cursor-text rounded-xl border-0 bg-primary-color p-3 text-secondary-color outline-none transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-primary-color"
-          type="email"
-          name="email"
-          placeholder="Your Email"
-        />
-        <textarea
-          className="my-3 h-44 w-full cursor-text rounded-xl border-0 bg-primary-color p-3 text-secondary-color outline-none transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-primary-color"
-          name="message"
-          placeholder="Your Message"
-        ></textarea>
-        <button
-          className="mt-8 w-full cursor-pointer rounded-xl border-0 bg-secondary-color py-4 px-8 font-medium text-white-color outline-none hover:bg-[#2430af] md:w-1/2"
-          type="button"
-        >
-          Send Message
-        </button>
-      </div>
+      {!isFormSubmitted ? (
+        <div className="mt-8 flex w-full flex-col items-center justify-center py-4 px-8 md:w-3/5">
+          <input
+            className="my-3 w-full cursor-text rounded-xl border-0 bg-primary-color p-3 text-secondary-color outline-none transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-primary-color"
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Your Name"
+            onChange={handleChangeInput}
+          />
+          <input
+            className="my-3 w-full cursor-text rounded-xl border-0 bg-primary-color p-3 text-secondary-color outline-none transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-primary-color"
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Your Email"
+            onChange={handleChangeInput}
+          />
+          <textarea
+            className="my-3 h-44 w-full cursor-text rounded-xl border-0 bg-primary-color p-3 text-secondary-color outline-none transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-primary-color"
+            name="message"
+            value={message}
+            placeholder="Your Message"
+            onChange={handleChangeInput}
+          />
+          <button
+            className="mt-8 w-full cursor-pointer rounded-xl border-0 bg-secondary-color py-4 px-8 font-medium text-white-color outline-none hover:bg-[#2430af] md:w-1/2"
+            type="button"
+            onClick={handleSubmit}
+          >
+            {!loading ? 'Send Message' : 'Sending...'}
+          </button>
+        </div>
+      ) : (
+        <h3 className="my-16 text-xl font-semibold text-secondary-color">
+          Thank you for getting in touch!
+        </h3>
+      )}
     </section>
   );
 };
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-  projectId: 'lwdckjm2',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2023-04-03',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   ignoreBrowserTokenWarning: true,
 });
 
